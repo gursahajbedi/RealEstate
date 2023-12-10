e0c5f6c7f44c907f0f7e2e8018434eb5d5499341
Gursahaj Singh <gursahajsinghkhalsa@gmail.com>
2023-12-11 03:05:22 +0530

    links resolved finally

--- src/App.jsx
+++ src/App.jsx
@@ -19,48 +19,46 @@ function App() {
   return (
     <>
     <div>
-      
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/"><h1 className="display-3 fw-bold border-bottom border-4 pb-2 pt-2">RealEstate</h1></Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="ms-3 navbar-nav  me-auto my-2 my-lg-0 gap-4">
-            <li className="nav-item border-bottom border-4">
-              <Link className="nav-link active mt-3" aria-current="page" to="/"><h3>Listing</h3></Link>
-            </li>
-            {value.user && (<li className="nav-item border-bottom border-4">
-              <Link className="nav-link active mt-3" to="/contact"><h3>Contact</h3></Link>
-            </li>)}
-            {value.user && (<li className="nav-item border-bottom border-4">
-              <Link className="nav-link active mt-3" to="/enroll"><h3>Enroll</h3></Link>
-            </li>)}
-            {!value.user && (<li className="nav-item border-bottom border-4">
-              <Link className="nav-link active mt-3" to="/login"><h3>Login</h3></Link>
-            </li>)}
-            {!value.user && (<li className="nav-item border-bottom border-4">
-              <Link className="nav-link active mt-3" to="/register"><h3>Register</h3></Link>
-            </li>)}
-          </ul>
-          {value.user && (<form className="d-flex" role="search">
-                <input className="form-control me-2"  type="search" placeholder="Search" aria-label="Search"></input>
-                <button className="btn btn-lg btn-outline-success" type="submit">Search</button>
-          </form>)}
-        </div>
-      </div>
-      {value.user &&(<button className="btn btn-lg btn-danger" onClick={Submitlogout}>Logout</button>)}
-    </nav>
-
       <Router>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+          <div className="container-fluid">
+            <Link className="navbar-brand" to="/"><h1 className="display-3 fw-bold border-bottom border-4 pb-2 pt-2">RealEstate</h1></Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul className="ms-3 navbar-nav  me-auto my-2 my-lg-0 gap-4">
+                <li className="nav-item border-bottom border-4">
+                  <Link className="nav-link active mt-3" aria-current="page" to="/"><h3>Listing</h3></Link>
+                </li>
+                {value.user && (<li className="nav-item border-bottom border-4">
+                  <Link className="nav-link active mt-3" to="/contact"><h3>Contact</h3></Link>
+                </li>)}
+                {value.user && (<li className="nav-item border-bottom border-4">
+                  <Link className="nav-link active mt-3" to="/enroll"><h3>Enroll</h3></Link>
+                </li>)}
+                {!value.user && (<li className="nav-item border-bottom border-4">
+                  <Link className="nav-link active mt-3" to="/login"><h3>Login</h3></Link>
+                </li>)}
+                {!value.user && (<li className="nav-item border-bottom border-4">
+                  <Link className="nav-link active mt-3" to="/register"><h3>Register</h3></Link>
+                </li>)}
+              </ul>
+              {value.user && (<form className="d-flex" role="search">
+                    <input className="form-control me-2"  type="search" placeholder="Search" aria-label="Search"></input>
+                    <button className="btn btn-lg btn-outline-success" type="submit">Search</button>
+              </form>)}
+            </div>
+          </div>
+          {value.user &&(<button className="btn btn-lg btn-danger" onClick={Submitlogout}>Logout</button>)}
+        </nav>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/login" element={!value.user?<Login/>:<Navigate to="/"/>} />
           <Route path="/register" element={!value.user?<Register/>:<Navigate to="/"/>}/>
           <Route path="/property" element={value.user?<Property/>:<Navigate to="/login"/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/enroll" element={<Enroll/>}/>
+          <Route path="/contact" element={value.user?<Contact/>:<Navigate to="/login"/>}/>
+          <Route path="/enroll" element={value.user?<Enroll/>:<Navigate to="/login"/>}/>
           
         </Routes>
       </Router>
