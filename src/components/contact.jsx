d0623f05ef9bd7aeb18920f7599c49b8f7e62c13
Gursahaj Singh <gursahajsinghkhalsa@gmail.com>
2023-12-11 11:21:20 +0530

    Update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,7 +10,7 @@ export default function Contact(){
 
     const submitContact =async(e)=>{
         e.preventDefault()
-        axios.post('https://realest-estate-the-website.onrender.com/api/contact/',{name,email,subject,message}).then((res)=>{
+        axios.post('https://localhost:8000/api/contact/',{name,email,subject,message}).then((res)=>{
             console.log("Message Sent")
             setconfirm(true)
         }).catch((err)=>{
