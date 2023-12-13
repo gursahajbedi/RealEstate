e8600b2a390f157880299a607200e5c5139508cf
Gursahaj Singh <gursahajsinghkhalsa@gmail.com>
2023-12-13 15:54:28 +0530

    V2.1 Resolved

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,13 +1,15 @@
 import { useContext } from "react"
 import useAuthContext from "../context/useAuthContext"
-
+import { WishlistContext } from "../context/WishlistContext"
 
 
 export default function useLogout(){
     const {dispatchs}=useAuthContext()
+    const {dispatch}=useContext(WishlistContext)
     const logout=async()=>{
         //removing user form the local storage
         localStorage.removeItem("user")
+        dispatch({type:"LOAD",payload:[]})
         localStorage.removeItem("wishlistArr")
         //dispatching null value
         await dispatchs({type:"logout"})
